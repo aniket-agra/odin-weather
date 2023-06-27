b0f94523714e53f5c1ee56d5fa57399db4ff60bb
Aniket Agrawal <aniket.mpa@gmail.com>
2023-06-27 21:21:23 +0200

    Refactor code to better separate functionality
    Now the call API functions can be used to set the values of weather
    and gif data. They can also be used to create promises on whose
    resolution we can access the values in weather and gif data. This
    can allow exporting of data from this module allowing this module
    to only send data.

--- src/data.js
+++ src/data.js
@@ -1,29 +1,32 @@
 function getData() {
+    let weatherData, gifData;
     let WEATHER_API_KEY = "98027771108a4e34ae895533232506";
     let GIPHY_API_KEY = "S1oe8Qn3VykKvfxVnEATGF24DHPFOaxk";
     let weatherBase = "http://api.weatherapi.com/v1/";
-    let giphyBase = "http://api.giphy.com/v1/gifs/search";
-    const callApi = function (qType, qParam) {
-        // let fetch_url = `${weatherBase}${qType}.json?key=${WEATHER_API_KEY}&q=${qParam}`;
-        let fetch_url = `${giphyBase}?api_key=${GIPHY_API_KEY}&q=${qParam}`;
+    let giphyBase = "http://api.giphy.com/v1/gifs/";
+    const callWeatherAPI = function (qType, qParam) {
+        let fetch_url = `${weatherBase}${qType}.json?key=${WEATHER_API_KEY}&q=${qParam}`;
         console.log(fetch_url);
         fetch(fetch_url, {mode : "cors"})
         .then((response) => {return response.json()})
-        .then((data) => {populateDisplay(data)}).catch((err) => console.error("Caught error: " + err));
+        .then((data) => {setWeatherData(data)}).catch((err) => console.error("Caught error: " + err));
     }
-    const getCurrent = function () {
-
-    };
-    const getForecast = function () {
-
-    };
-    function populateDisplay(data) {
+    const callGiphyAPI = function (qType, qParam) {
+        let fetch_url = `${giphyBase}${qType}?api_key=${GIPHY_API_KEY}&q=${qParam}`;
+        console.log(fetch_url);
+        fetch(fetch_url, {mode : "cors"})
+        .then((response) => {return response.json()})
+        .then((data) => {setGifData(data)}).catch((err) => console.error("Caught error: " + err));
+    }
+    function setWeatherData(data) {
         console.log(data);
+        weatherData = data;
     }
-    let data; 
-    // callApi("current", "london");
-    callApi("", "sunny");
-
+    function setGifData(data) {
+        console.log(data);
+        gifData = data;
+    }
+    return {weatherData, gifData};
 }
 
 export {getData};
