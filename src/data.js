b9e172e7308a5518aff70ea5b26cdcc57cfab8ab
Aniket Agrawal <agra.aniket@gmail.com>
2023-06-29 15:34:46 +0200

    Get data over https

--- src/data.js
+++ src/data.js
@@ -1,8 +1,8 @@
 function getData() {
     let WEATHER_API_KEY = "98027771108a4e34ae895533232506";
     let GIPHY_API_KEY = "S1oe8Qn3VykKvfxVnEATGF24DHPFOaxk";
-    let weatherBase = "http://api.weatherapi.com/v1/";
-    let giphyBase = "http://api.giphy.com/v1/gifs/";
+    let weatherBase = "https://api.weatherapi.com/v1/";
+    let giphyBase = "https://api.giphy.com/v1/gifs/";
     const callWeatherAPI = function (qType, qParam) {
         let fetch_url = `${weatherBase}${qType}.json?key=${WEATHER_API_KEY}&q=${qParam}`;
         console.log(fetch_url);
