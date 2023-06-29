d09dd7e834bb2483c2eb1efec5857f8c98dc3cbb
Aniket Agrawal <agra.aniket@gmail.com>
2023-06-29 15:24:56 +0200

    Group page elements into blocks for better readability

--- src/page.js
+++ src/page.js
@@ -6,33 +6,37 @@ function populatePage() {
     document.querySelector("head").appendChild(iconElem);
 
     let bodyElem = document.querySelector("body");
-
-    let searchDiv = document.createElement("div");
-    searchDiv.classList.add("search");
-    let labelElem = document.createElement("label");
-    labelElem.setAttribute("for", "location");
-    labelElem.textContent = "Enter location: "
-    let inputElem = document.createElement("input");
-    let inputAttr = {"type" : "text", "name" : "location", "id" : "location", "value" : "london"};
-    for(let key in inputAttr)
-        inputElem.setAttribute(key, inputAttr[key]);
-    inputElem.addEventListener("change", function (e) {
-        inputElem.setAttribute("value", e.target.value);        
-    });
-    let searchBtn = document.createElement("div");
-    searchBtn.classList.add("findBtn");
-    searchBtn.classList.add("material-symbols-outlined");
-    searchBtn.style.fontVariationSettings = "'FILL' 0, 'wght' 400, 'GRAD' 200, 'opsz' 48";
-    searchBtn.style.fontSize = "1.1rem";
-    searchBtn.textContent = "search";
-    searchDiv.appendChild(labelElem);
-    searchDiv.appendChild(inputElem);
-    searchDiv.appendChild(searchBtn);
-    bodyElem.appendChild(searchDiv);
-
-    let weatherDiv = document.createElement("div");
-    weatherDiv.classList.add("weatherText");
-    bodyElem.appendChild(weatherDiv);
+    {
+        let searchDiv = document.createElement("div");
+        searchDiv.classList.add("search");
+        let labelElem = document.createElement("label");
+        labelElem.setAttribute("for", "location");
+        labelElem.textContent = "Enter location: "
+        let inputElem = document.createElement("input");
+        let inputAttr = {"type" : "text", "name" : "location", "id" : "location", "value" : "london"};
+        for(let key in inputAttr)
+            inputElem.setAttribute(key, inputAttr[key]);
+        inputElem.addEventListener("change", function (e) {
+            inputElem.setAttribute("value", e.target.value);        
+        });
+        let searchBtn = document.createElement("div");
+        searchBtn.classList.add("findBtn");
+        searchBtn.classList.add("material-symbols-outlined");
+        searchBtn.style.fontVariationSettings = "'FILL' 0, 'wght' 400, 'GRAD' 200, 'opsz' 48";
+        searchBtn.style.fontSize = "1.1rem";
+        searchBtn.textContent = "search";
+        searchDiv.appendChild(labelElem);
+        searchDiv.appendChild(inputElem);
+        searchDiv.appendChild(searchBtn);
+        bodyElem.appendChild(searchDiv);
+    }
+    
+    {
+        let weatherDiv = document.createElement("div");
+        weatherDiv.classList.add("weatherText");
+        bodyElem.appendChild(weatherDiv);
+    }
+    
 }
 
 export {populatePage}
