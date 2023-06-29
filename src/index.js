b4715a197b4212385fefc0b74fbcc3d981647686
Aniket Agrawal <agra.aniket@gmail.com>
2023-06-29 15:36:12 +0200

    Comment test code

--- src/index.js
+++ src/index.js
@@ -22,10 +22,10 @@ searchBtn.addEventListener("click", function (e) {
     });
 });
 
-let imgElem = document.createElement("img");
-dataFetcher.callGiphyAPI("random", "cat")
-.then((data) => {
-    imgElem.setAttribute("src", `${data["data"]["images"]["original"]["url"]}`);
-})
-.catch(err => alert(err));
+// let imgElem = document.createElement("img");
+// dataFetcher.callGiphyAPI("random", "cat")
+// .then((data) => {
+//     imgElem.setAttribute("src", `${data["data"]["images"]["original"]["url"]}`);
+// })
+// .catch(err => alert(err));
 // document.querySelector("body").appendChild(imgElem);
