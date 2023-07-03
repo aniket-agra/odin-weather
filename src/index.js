6a610b1c87ee2389b5a2c7cbf9a0da45143c2127
Aniket Agrawal <agra.aniket@gmail.com>
2023-07-03 11:55:27 +0200

    Add GIF from giphy

--- src/index.js
+++ src/index.js
@@ -9,16 +9,26 @@ let dataFetcher = getData();
 let searchBtn = document.querySelector(".findBtn");
 searchBtn.addEventListener("click", function (e) {
     let weatherDiv = document.querySelector(".weatherText");
+    let imgDiv = document.querySelector(".weatherGIF");
+    let weatherCondition;
     let location = document.querySelector("input#location").getAttribute("value");
     dataFetcher.callWeatherAPI("current", location)
     .then((data) => {
         console.log(data);
-        let displayText = `In ${data["location"]["name"]}, it is currently ${data["current"]["condition"]["text"]}`;
+        weatherCondition = data["current"]["condition"]["text"];
+        let displayText = `In ${data["location"]["name"]}, it is currently ${weatherCondition}`;
         displayText += ` with a temperature of ${data["current"]["temp_c"]} degrees.\n`;
         if (data["current"]["temp_c"] > 20) {
             displayText += "Finally! No jackets!!!";
         }
         weatherDiv.textContent = displayText;
+    })
+    .then(() => {
+        dataFetcher.callGiphyAPI("random", weatherCondition.split(" ").join("+"))
+        .then((data) => {
+            imgDiv.setAttribute("alt", weatherCondition);
+            imgDiv.setAttribute("src", `${data["data"]["images"]["fixed_height"]["url"]}`);
+        });
     });
 });
 
