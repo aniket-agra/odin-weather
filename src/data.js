4c2cf7a8f3b866935e15907adc8ffbb5e4fe8973
Aniket Agrawal <aniket.mpa@gmail.com>
2023-07-24 21:50:08 +0200

    Encapsulate url info inside function

--- src/data.js
+++ src/data.js
@@ -1,15 +1,15 @@
 function getData() {
     let WEATHER_API_KEY = "98027771108a4e34ae895533232506";
     let GIPHY_API_KEY = "S1oe8Qn3VykKvfxVnEATGF24DHPFOaxk";
-    let weatherBase = "https://api.weatherapi.com/v1/";
-    let giphyBase = "https://api.giphy.com/v1/gifs/";
     const callWeatherAPI = function (qType, qParam) {
+        let weatherBase = "https://api.weatherapi.com/v1/";
         let fetch_url = `${weatherBase}${qType}.json?key=${WEATHER_API_KEY}&q=${qParam}`;
         console.log(fetch_url);
         return fetch(fetch_url, {mode : "cors"})
         .then((response) => {return response.json()});
     }
     const callGiphyAPI = function (qType, qParam) {
+        let giphyBase = "https://api.giphy.com/v1/gifs/";
         let fetch_url = `${giphyBase}${qType}?api_key=${GIPHY_API_KEY}&tag=${qParam}`;
         console.log(fetch_url);
         return fetch(fetch_url, {mode : "cors"})
