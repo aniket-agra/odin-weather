f39c179ccc4fbe0fdaafb1a958d5c858fe98bda8
Aniket Agrawal <agra.aniket@gmail.com>
2023-07-27 13:04:25 +0200

    Change news source for better headlines

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ function getData() {
     let WEATHER_API_KEY = "98027771108a4e34ae895533232506";
     let GIPHY_API_KEY = "S1oe8Qn3VykKvfxVnEATGF24DHPFOaxk";
     let AQICN_API_KEY = "b948e0531f21bfdc57f17b820550ec7a01c14f4b";
-    let NEWS_API_KEY = "pub_26755878d643551ced9b8ae00c63c783bcf35";
+    let NEWS_API_KEY = "d0bde101400b31138dcc1b588877ba1b";
     const callWeatherAPI = function (qType, qParam) {
         let weatherBase = "https://api.weatherapi.com/v1/";
         let fetch_url = `${weatherBase}${qType}.json?key=${WEATHER_API_KEY}&q=${qParam}&aqi=yes`;
@@ -25,8 +25,9 @@ function getData() {
         return data;
     }
     const callNewsAPI = async function (qParams) {
-        let base = "https://newsdata.io/api/1/news?";
-        let fetch_url = base + `apikey=${NEWS_API_KEY}&qInTitle=${qParams}`;
+        let base = "http://api.mediastack.com/v1/news?";
+        let fetch_url = base + `access_key=${NEWS_API_KEY}&keywords=${qParams}` +
+                        `&date=${new Date().toISOString().split('T')[0]}`;
         let response = await fetch(fetch_url, {mode: "cors"});
         let data = await response.json();
         return data;
